94121fc5e2e470272d38a19f0cb977cae0d6b369
DrAP <agus10pg@gmail.com>
2018-02-07 00:49:18 +0100

    Mirar atras y cambios en terreno

---
+++ Assets/MirarAtras.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MirarAtras : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+
+    }
+}
